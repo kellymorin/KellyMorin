589f188e077df0148a41d9ab75d3c5aa9b64427c
kellymorin <kmorin06@gmail.com>
2019-01-30 20:48:30 -0600

    Mostly finalized portfolio website template. See todo items for outstanding tasks.

---
+++ assets/js/scripts.js
@@ -0,0 +1,48 @@
+
+$('#NSS-Info-Switch').click(function(){
+  let button_copy = $('#NSS-Info-Switch').text()
+  if(button_copy === 'Read More'){
+    $('#NSS-Info-Switch').text('Read Less')
+  } else{
+    $('#NSS-Info-Switch').text('Read More')
+  }
+})
+
+$('#OBC-Info-Switch').click(function(){
+  let button_copy = $('#OBC-Info-Switch').text()
+  if(button_copy === 'Read More'){
+    $('#OBC-Info-Switch').text('Read Less')
+  } else{
+    $('#OBC-Info-Switch').text('Read More')
+  }
+})
+
+$('#RVH-Info-Switch').click(function(){
+  let button_copy = $('#RVH-Info-Switch').text()
+  if(button_copy === 'Read More'){
+    $('#RVH-Info-Switch').text('Read Less')
+  } else{
+    $('#RVH-Info-Switch').text('Read More')
+  }
+})
+
+$('#CHM-Info-Switch').click(function(){
+  let button_copy = $('#CHM-Info-Switch').text()
+  if(button_copy === 'Read More'){
+    $('#CHM-Info-Switch').text('Read Less')
+  } else{
+    $('#CHM-Info-Switch').text('Read More')
+  }
+})
+
+$('#ICG-Info-Switch').click(function(){
+  let button_copy = $('#ICG-Info-Switch').text()
+  if(button_copy === 'Read More'){
+    $('#ICG-Info-Switch').text('Read Less')
+  } else{
+    $('#ICG-Info-Switch').text('Read More')
+  }
+})
+
+{/* <a class="experience-collapse" data-toggle="collapse" data-target="#NSS-Info" aria-expanded="false" id="NSS-Info-Switch" aria-controls="collapseExample">Read More</a> */}
+{/* <div class="collapse" id="NSS-Info"> */}
